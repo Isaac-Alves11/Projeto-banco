feca0fb2aed26128e67f1ae485b817e5130a7b3c
Isaac Alves <Isaac.alves11@outlook.com>
2023-06-06 11:02:04 -0300

    Adicionei novas classes

---
+++ index.js
@@ -0,0 +1,26 @@
+class cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+new cliente();
+
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+
+const cliente3Nome = "Geraldo";
+const cliente3CPF = 99922233309;
+const cliente3RG = assa
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+console.log(cliente1Nome);
