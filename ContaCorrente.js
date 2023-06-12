a79e172fd567810631976121ac12f51d70c97c7f
Isaac Alves <Isaac.alves11@outlook.com>
2023-06-12 15:07:18 -0300

    Adicionei nova instância

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,17 @@
-import class contaCorrente{
+export class contaCorrente{
     agencia;
+    cliente;
+
+    set cliente(novoValor){
+        if (novoValor instanceof cliente)
+       this._cliente = novoValor;
+
+    }
+
+
+
     //#saldo para campos privados! https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo;
+    _saldo = 0;
 
     sacar(valor){
         if(this.saldo >= valor){
@@ -16,4 +26,11 @@ import class contaCorrente{
         }
             this.saldo += valor;
     }
-}
+
+    transferir(valor, conta) {
+        const valorSacado  = this.sacar(valor);
+        conta.depositar(valorSacado);
+        conta.cidade = "São Paulo";
+
+    }
+} 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {cliente} from "./Cliente.js"
-import (contaCorrente) from "./ContaCorrente.js"
+import {contaCorrente} from "./ContaCorrente.js"
 
 
 const cliente1 = new cliente();
@@ -7,17 +7,26 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.rg = 123456789;
 
-const cliente2 = new cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+//const cliente2 = new cliente();
+//cliente2.nome = "Alice";
+//cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new contaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(200);
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+const conta2 = new contaCorrente();
+conta2.cliente = new cliente();
+conta2.cliente.nome = "Alice";
+conta2.cpf = 88822233309
+conta2.agencia = 102;
 
+let valor = 200;
+contaCorrenteRicardo.transferir(200, conta2);
+
+
+console.log(valor);
+console.log(conta2);
 console.log(contaCorrenteRicardo);
